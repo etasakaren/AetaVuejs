27d87e1b0fe01a7b4f41d0845d34f822906883a2
Etasa K <ekarenisa@yahoo.co.id>
2020-07-31 21:48:12 +0700

    Added REST Api

---
+++ vue-demo/src/main.js
@@ -0,0 +1,25 @@
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'bootstrap '
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+    // Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+})
